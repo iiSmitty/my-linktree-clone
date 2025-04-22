a9923a8d538ca1244ac46e5ea7d1fd50c8c76094
André Smit <andrez.smit@gmail.com>
2025-04-22 22:47:01 +0200

    Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://iiSmitty.github.io',
+  // No 'base' needed for root deployment
+});
