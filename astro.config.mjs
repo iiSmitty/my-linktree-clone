a5079d54ec9df9b07640915aff58a24cef30f13b
André Smit <andrez.smit@gmail.com>
2025-04-22 23:29:46 +0200

    Configure custom domain linktr.andresmit.co.za

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iismitty.github.io',
-  base: '/my-linktree-clone/',     
+  site: 'https://linktr.andresmit.co.za',
 });
